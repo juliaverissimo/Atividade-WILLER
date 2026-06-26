0aa5829a1ebcd386c1bd57136756a2f6b363feae
JÚLIA VERISSIMO <julia_verissimo150@estudante.sesisenai.org.br>
2026-06-26 08:51:58 -0300

    criação de rotas

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from "express"
 import { prisma } from "./lib/prisma.ts"
+import cors from "cors"
 
 const app = express()
-const PORT = 3000
-app.get("/", async (req, res) =>{
+app.use(cors())
+app.use(express.json())
 
-    const user = await prisma.user.findMany()
-    res.json(user)
-})
+const PORT = 3000
+
+app.get("/produtos", async (req, res) =>{ 
+try{
+        const itens = await prisma.produtos.findMany()
+        res.json(itens)
+
+    } catch(error){
+        res.status(500).json({error: "Erro ao buscar produtos"})
+    }
+});
 
 app.listen(PORT, () => {
-  console.log("servidor rodando"); // Isso é uma mensagem de log, NÃO um endereço
+  console.log("API subida");
 });
