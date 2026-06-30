09eead4e7ea7a3a1491b1867394a21e3c4b854f6
JÚLIA VERISSIMO <julia_verissimo150@estudante.sesisenai.org.br>
2026-06-30 08:34:20 -0300

    atualização 2

--- src/server.js
+++ src/server.js
@@ -8,15 +8,28 @@ app.use(express.json())
 
 const PORT = 3000
 
-app.get("/produtos", async (req, res) =>{ 
-try{
-        const itens = await prisma.produtos.findMany()
-        res.json(itens)
+app.get("/produtos", async (req, res) => {
+  try {
+    const itens = await prisma.produtos.findMany()
+    res.json(itens)
 
-    } catch(error){
-        res.status(500).json({error: "Erro ao buscar produtos"})
-    }
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar produtos" })
+  }
 });
+
+aoopost("/produtos", async (req, rs) => {
+  const { none, categoria, quantidade } = req.body
+  try {
+    const novoItem = await prisma.estoque.create({
+      data: { nome, categoria, quantidade: Number(quantidade) }
+    });
+
+    res.status(201).json(novoItem)
+  } catch (error) {
+    res.status(400).json({ error: "Erro ao criar produto" })
+  }
+})
 
 app.listen(PORT, () => {
   console.log("API subida");
